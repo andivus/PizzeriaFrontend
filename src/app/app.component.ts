0962c2c99a7ab75bac08c0afe13bd406af8ca100
Andiv <div2005.kk@gmail.com>
2023-06-11 08:49:54 +0300

    Initial 2 Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {AddEditItemComponent} from "./add-edit-item/add-edit-item.component";
+import {ItemService} from "./service/item.service";
+import ItemDTO from "./model/item-dto";
+import {MatSort} from "@angular/material/sort";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,88 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pizzeria';
+
+  displayedColumns: string[] = ['id', 'image', 'name', 'price', 'stock', 'isActive', 'editButton'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private _dialog: MatDialog,
+    private _itemService: ItemService,
+    private _snackBar: MatSnackBar,
+  ) {}
+
+  ngOnInit(): void {
+    this.getItemList()
+
+  }
+
+  openAddItemDialog(): void {
+    const dialogRef = this._dialog.open(AddEditItemComponent, {
+      width: '600px',
+    })
+
+    dialogRef.afterClosed().subscribe({
+      next: (value) => {
+        if (value) {
+          this.getItemList()
+        }
+    }
+    })
+  }
+
+  openEditItemDialog(data: any) {
+    const dialogRef = this._dialog.open(AddEditItemComponent, {
+      width: '600px',
+      data
+    })
+
+    dialogRef.afterClosed().subscribe({
+      next: (value) => {
+        if (value) {
+          this.getItemList()
+        }
+      }
+    })
+  }
+
+  getItemList() {
+    this._itemService.getItems().subscribe({
+      next: (val: Array<ItemDTO>) => {
+        val.sort((a, b) => a.id - b.id)
+        this.dataSource = new MatTableDataSource<any>(val)
+        this.dataSource.sort = this.sort
+        this.dataSource.paginator = this.paginator
+      },
+      error: console.error
+      })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  onDeleteClick(id: number): void {
+    this._itemService.removeItem(id).subscribe({
+      next: (val: ItemDTO) => {
+        this.getItemList()
+        this._snackBar.open(`Товар #${val.id} успішно видалено!`, 'Окей', {
+          duration: 5000
+        });
+      },
+      error: (val: any) => {
+        this._snackBar.open('Виникла помилка при видаленні товару!', 'Окей', {
+          duration: 5000
+        });
+        console.error(val)
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import { EditItemComponent } from './edit-item/edit-item.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatInputModule} from "@angular/material/input";
+import { AddEditItemComponent } from './add-edit-item/add-edit-item.component';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatChipsModule} from "@angular/material/chips";
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSortModule} from "@angular/material/sort";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditItemComponent,
+    AddEditItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatChipsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
